ad3fbc51449644b24944cae67ec7c4b2891b2b28
Steven Fransen <sf258g@att.com>
2017-08-29 12:47:52 -0700

    add build script

---
+++ buildrun.sh
@@ -0,0 +1,3 @@
+/opt/local/bin/mvn compile -Dcheckstyle.skip -Dmaven.javadoc.skip=true
+/opt/local/bin/mvn test -Dcheckstyle.skip -Dmaven.javadoc.skip=true
+/opt/local/bin/mvn  -Dcheckstyle.skip -Dmaven.javadoc.skip=true hpi:run
